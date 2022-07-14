c53c839791df52ad60c2153a9a921c0f22265cd6
Frank Doyle <frankd@Franks-MacBook-Pro-2.local>
2022-07-14 12:20:27 -0500

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,55 @@
+<script lang="js">
+	// @ts-ignore
+	import QR from 'svelte-qr';
+import { each } from 'svelte/internal';
+	import { v4 as uuidv4 } from 'uuid';
+
+	let uuid = uuidv4();
+	let history = [uuid];
+
+	function newUuid() {
+		uuid = uuidv4();
+		history = [uuid].concat(history)
+	}
+
+	/**
+* @param {string} newUuid
+*/
+	function setCurrent(newUuid) {
+		uuid = newUuid
+	}
+</script>
+
+<div class="container">
+	<title>QR code generator</title>
+
+	<img class="qr" src="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data={uuid}"/>
+
+	<p>{uuid}</p>
+
+    <button on:click={newUuid} aria-label="new uuid"> New UUID </button>
+
+	{#each history as item (item)}
+		<a class="history-item" on:click={() => {setCurrent(item)}}>{item}</a>
+	{/each}
+
+</div>
+
+<style>
+	.qr {
+		width: 300px;
+	}
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items:center;
+    }
+	
+	.history-item {
+		font-family: 'Courier New', Courier, monospace;
+		font-weight: bold;
+		margin: 3px;
+	}
+</style>
